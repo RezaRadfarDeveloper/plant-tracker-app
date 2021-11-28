a7bbdc1ca7b5b40bc2230fbfd9fefd6a098e1b68
Reza Radfar <reza.radfar@inspiringvacations.com>
2021-11-29 03:53:06 +1100

    adds index view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <style>
-    @yield('css')
+    @yield('custom_css')
     </style>
 </head>
 <body class="antialiased">
